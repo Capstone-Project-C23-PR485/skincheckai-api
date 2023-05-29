c1de59a0fa1cfb080cad936a3beca6a5c8832c3a
tridims <tri.dimas@student.ub.ac.id>
2023-05-29 11:58:43 +0700

    update user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUserDto {}
+import { IsDate, IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  uid: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsDate()
+  birthDate: Date;
+
+  @IsNotEmpty()
+  photo: string;
+
+  @IsNotEmpty()
+  skinType: string;
+
+  @IsNotEmpty()
+  skinCondition: string;
+}
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
-
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  name: string;
+  birthDate: Date;
+  skinType: string;
+  skinCondition: string;
+}
