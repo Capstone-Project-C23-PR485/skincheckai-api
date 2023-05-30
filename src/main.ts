5f91dbff6c08ee2f18b006413b1a6e9060c462a8
tridims <tri.dimas@student.ub.ac.id>
2023-05-30 19:07:35 +0700

    Adding swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('SkinCheck.AI API Endpoints')
+    .setDescription('The SkinCheck.AI API description')
+    .setVersion('1.0')
+    .addTag('User')
+    .addTag('Image Analysis')
+    .addTag('Products')
+    .addTag('Logs')
+    .addTag('Machine Learning')
+    .addTag('Internal Use')
+    .addTag('Tips')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
