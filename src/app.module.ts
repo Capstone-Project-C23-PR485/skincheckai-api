135b350c161f8ddde9983a388fe73fa7e29ef4c9
tridims <tri.dimas@student.ub.ac.id>
2023-05-30 18:30:32 +0700

    importing module for firbase auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { FirebaseAuthModule } from '@whitecloak/nestjs-passport-firebase';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot(),
+    FirebaseAuthModule.register({
+      audience: 'testing-auth-with-node',
+      issuer: 'https://securetoken.google.com/testing-auth-with-node',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
